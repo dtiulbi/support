9d618d0792379399eac1f00fa4fef5b09ca92b04
christyuda <yudacihuy09@gmail.com>
2023-06-14 00:11:31 +0700

    support tik add issues

--- js/controller/issues.js
+++ js/controller/issues.js
@@ -3,7 +3,7 @@ import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 
 document.addEventListener('DOMContentLoaded', () => {
     const octokit = new Octokit({
-      auth: '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'
+      auth: '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'
     });
 
     const form = document.getElementById('issuesform');
@@ -13,9 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const title = document.getElementById('inputtittle').value;
       const body = document.getElementById('inputbody').value;
-      const assignees = [document.getElementById('inputassigness').value];
       const milestone = parseInt(document.getElementById('inputmilestone').value);
-      const labels = [document.getElementById('inputlabels').value];
+      const assigneeSelect = document.getElementById('inputassigness');
+      const assignees = [assigneeSelect.options[assigneeSelect.selectedIndex].value];
+    
+      const labelsSelect = document.getElementById('inputlabels');
+      const labels = Array.from(labelsSelect.selectedOptions, option => option.value);
+      
 
       try {
         const response = await octokit.request('POST /repos/dtiulbi/questionstik/issues', {
@@ -31,8 +35,33 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         });
         console.log('Issue created:', response);
+        showModal('Pengajuan DiProses', response);
+
       } catch (error) {
         console.error('Failed to create issue:', error);
+        showModalerror('Gagal Mengirim Pengajuan Pastikan Sudah Diisi Semua', error);
+
       }
     });
   });
+
+  function showModal(message) {
+    Swal.fire({
+      title: 'Selamat!',
+      text: message,
+      icon: 'success',
+      confirmButtonText: 'Tutup'
+    }).then(() => {
+      location.reload();
+    });
+  }
+
+  function showModalerror(message) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Gagal Mengirim Pengajuan Pastikan Sudah Diisi Semua',
+    }).then(() => {
+      location.reload();
+    });
+  }
