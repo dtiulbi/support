865272b76e2b0a5a8e160a2ccad88912bbce51f0
Rolly Maulana Awangga <rolly@awangga.net>
2023-06-20 16:02:55 +0700

    markicob

--- js/controller/issues.js
+++ js/controller/issues.js
@@ -1,55 +1,31 @@
-
-import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
 import Swal from "https://cdn.skypack.dev/sweetalert2@11.1.2";
+import {onClick,getValue} from "https://jscroot.github.io/element/croot.js";
+import {postWithBearer} from "https://jscroot.github.io/api/github.js";
+import { iteungbot } from "./bot.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    const uhuy = 'Z2hwX2lmVW94RGVyaWdxWDdicXg4RzdFNklMczlQTUtGVTJnaVlZZg==';
-    const octokit = new Octokit({
-      auth: atob(uhuy)
-    });
+onClick("submitButton",kirimIssues);
 
-    const form = document.getElementById('issuesform');
+function kirimIssues(){
+  let data = {
+    title : getValue("inputtittle"),
+    assignees : [getValue("inputassigness")],
+    milestone : Number(getValue("inputmilestone")),
+    labels : [getValue("inputlabels")],
+    body : getValue("inputbody"),
+  }
+  postWithBearer("https://api.github.com/repos/acadcsirt/lapor/issues",atob(iteungbot),data,responseFunction)
+}
 
-    form.addEventListener('submit', async (e) => {
-      e.preventDefault();
+function responseFunction(response){
+    console.log('Issue created:', response);
+    if(response.message === "Validation Failed"){
+      showModalerror('Gagal Mengirim Laporan Pastikan Sudah Diisi Semua ' + response.errors[0].code);
+    } else{
+      showModal('ID Laporan ' + response.id);
+    }
+}
 
-      const title = document.getElementById('inputtittle').value;
-      const body = document.getElementById('inputbody').value;
-      const milestone = parseInt(document.getElementById('inputmilestone').value);
-      const assigneeSelect = document.getElementById('inputassigness');
-      const assignees = [assigneeSelect.options[assigneeSelect.selectedIndex].value];
-    
-      const labelsSelect = document.getElementById('inputlabels');
-      const labels = Array.from(labelsSelect.selectedOptions, option => option.value);
-      
-
-      try {
-        const response = await octokit.request('POST /repos/dtiulbi/question-ticketing/issues', {
-          owner: 'dtiulbi',
-          repo: 'question-ticketing',
-          title,
-          assignees,
-          milestone,
-          labels,
-          body,
-          headers: {
-            'X-GitHub-Api-Version': '2022-11-28',
-            'Accept': 'application/vnd.github.v3+json'
-
-          }
-        });
-        console.log('Issue created:', response);
-        showModal('Pengajuan Tiket Layanan Akan Proses', response);
-
-      } catch (error) {
-        console.error('Failed to create issue:', error);
-        showModalerror('Gagal Mengirim Pengajuan Pastikan Sudah Diisi Semua', error);
-
-      }
-    });
-  });
-
-  function showModal(message) {
+function showModal(message) {
     Swal.fire({
       title: 'Selamat!',
       text: message,
@@ -60,7 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  function showModalerror(message, error) {
+function showModalerror(message, error) {
     Swal.fire({
       icon: 'error',
       title: 'Oops...',
